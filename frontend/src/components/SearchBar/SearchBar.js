11b667428eaa42f830db247d62c7a834a5136bcb
Cyrus Gandevia <cyrus.gandevia@gmail.com>
2021-04-05 08:41:06 -0400

    [Frontend]: Integrated SearchBar Component with Backend

--- frontend/src/components/SearchBar/SearchBar.js
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React, {useEffect, useState} from 'react';
 import { withRouter } from 'react-router-dom';
-
-//import axios from 'axios';
+import axios from 'axios';
 
 import {
   StyledSearch,
@@ -9,62 +8,57 @@ import {
   DropdownIndicator
 } from './styles';
 
-import companyData from './junkSearch.json';
+const SearchBar = (props) => {
+  const [selectedCompany, setSelectedCompany] = useState(null);
+  const [companyData, setCompanyData] = useState({});
 
+  useEffect(() => {
+    getCompanyData();
+  }, []);
 
-//GET COMPANY DATA FOR SEARCH BAR
-// async function getData(){
-//     try {
-//       const responseData = await axios.get('http://localhost:5000/data/companyData');
-//       console.log(responseData);  
-//     }
-//     catch (error) {
-//       console.log('error: ' + error);
-//     }
-//   }
+  const getCompanyData = async () => {
+    //TODO: Export backend route variables into a standalone constants file and call from there
+    const companyDataURL = "http://localhost:5000/data/companyData";
 
-// Maps placeholder data for companies currently
-const companyMap = companyData.map(
-    ({company_name}) => {
+    // TODO: Error handling for get request
+    const response = (await axios.get(companyDataURL)).data;
+
+    const listOfCompanies = response.map(
+      ({company_name}) => {
         return {
-            label: company_name,
-            value: company_name
+          label: company_name,
+          value: company_name
         }
-    }
-);
+      }
+    );
 
-// Search Bar Component
-class SearchBar extends Component {
-  state = {
-    selectedOption: null,
-  }
-
-  handleChange = (selectedOption) => {
-    this.setState({ selectedOption });
-
-    // Redirects the router to the selected company's page
-    const company = selectedOption.label;
-    this.props.history.push("/company/" + company);
-
-    //TODO: (Will need the search bar present even on company pages in case users want to search for another company)
+    setCompanyData(listOfCompanies);
   };
 
-  render() {
-    return (
-      <div>
-        <StyledSearch
-          value={this.state.selectedOption}       // Allows for selected option to appear in search bar, after clicking it
-          options={companyMap}                    // Uses the map to display the given options
-          onChange={this.handleChange}  
-          placeholder="Search for a company..."   
-          openMenuOnClick={false}                 // Prevents option to reveal all options when user clicks the search bar
-          classNamePrefix="select"                // Since it is part of the "Select" component
-          styles={SearchBarStyles}                // Utilizes custom style given above
-          components={{ DropdownIndicator }}
-        />
-      </div>
-    );
-  }
-}
+  const handleChange = (selection) => {
+    setSelectedCompany(selection.label);
+
+    // Redirects the router to the selected company's page
+    const company = selection.label;
+    props.history.push("/company/" + company);
+
+    //TODO: Will need the search bar present even on company pages in case users want to search for another company
+  };
+
+  return (
+    <div>
+      <StyledSearch
+        value={selectedCompany} // Allows for selected option to appear in search bar, after clicking it
+        options={companyData} // Uses the map to display the given options
+        onChange={handleChange}
+        placeholder="Search for a company..."
+        openMenuOnClick={false} // Prevents option to reveal all options when user clicks the search bar
+        classNamePrefix="select" // Since it is part of the "Select" component
+        styles={SearchBarStyles} // Utilizes custom style given above
+        components={{ DropdownIndicator }}
+      />
+    </div>
+  );
+};
 
 export default withRouter(SearchBar);  
