dbd0e7137cd5ab2efa011b0a697b5e7e2a132b97
Birsana <andrebirsa2@gmail.com>
2021-04-15 22:09:07 -0400

    add company template method

--- frontend/src/pages/addCompany.js
+++ frontend/src/pages/addCompany.js
@@ -1,7 +1,27 @@
 import React from "react";
+import axios from 'axios';
 
 const addCompanyPage = (props) => {
   //TODO: Need some verification with backend here if user has been logged in
+
+  const addCompany = async () => {
+      let addURL = "http://localhost:5000/:companyname/addjob"; //replace :companyname with the actual company
+        axios({
+            method: 'post',
+            url: addURL,
+            data: {
+                    "job":{
+                      "job_name": "Software Engineer" //replace with desired job
+                    }
+                  },
+            headers: {
+                "Authorization": "Token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwMmVmNzU1YzI2M2NlMmU5ODA3MTczYiIsInVzZXJuYW1lIjoic2Vjb25kIiwiZXhwIjoxNjI0MDU1MTA5LCJpYXQiOjE2MTM2OTA3MDl9.R622UH4VjyF-bR_Has3ajC5fp0S4gIKG9u4OsuKxX5w",
+                "Content-Type": "application/json",
+                "X-Requested-With": "XMLHttpRequest"
+              }
+        })
+  }
+
   
   return (
     <div>
