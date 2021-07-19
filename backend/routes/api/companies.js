6b0a190ecc0b89bcbe96ea704d92a9259e02ecd9
Cyrus Gandevia <cyrus.gandevia@gmail.com>
2021-07-19 15:32:10 -0400

    Fixed add job backend call

--- backend/routes/api/companies.js
+++ backend/routes/api/companies.js
@@ -86,9 +86,9 @@ router.post('/:companyname/addjob', auth.required, function(req, res, next) {
       if(!user){ 
           return res.sendStatus(401); 
         }
-  
+
       var companyName = req.params.companyname;
-      var job = new Job(req.body.job.trim());
+      var job = new Job({jobName: req.body.job.jobName.trim()});
       job.addedBy = user.email;
       job.company = companyName;
       job.averageRating = 0;
